2d0649fdb337328f85bcbd378bd273c05efb5701
Honwaka Developer <honwaka.developer@gmail.com>
2019-08-04 10:20:09 +0900

    init commit

---
+++ app/src/main/java/dev/honwaka_lab/honpass/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package dev.honwaka_lab.honpass.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
