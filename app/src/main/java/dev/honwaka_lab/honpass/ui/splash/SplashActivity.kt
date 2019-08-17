4c4f27e9edce712c5bce9441dfa71e9475e4e7c5
Honwaka Developer <honwaka.developer@gmail.com>
2019-08-17 14:59:59 +0900

    スプラッシュ画面用のActivityを追加

---
+++ app/src/main/java/dev/honwaka_lab/honpass/ui/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package dev.honwaka_lab.honpass.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.honwaka_lab.honpass.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
