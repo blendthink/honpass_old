385a58e92b23dbe7d48259fbad1f8b0e6c541c4e
Honwaka Developer <honwaka.developer@gmail.com>
2019-08-20 12:07:23 +0900

    Splash画面からの遷移を仮実装

--- app/src/main/java/dev/honwaka_lab/honpass/ui/splash/SplashActivity.kt
+++ app/src/main/java/dev/honwaka_lab/honpass/ui/splash/SplashActivity.kt
@@ -1,13 +1,40 @@
 package dev.honwaka_lab.honpass.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import dev.honwaka_lab.honpass.R
+import dev.honwaka_lab.honpass.ui.register.RegisterActivity
 
 internal class SplashActivity : AppCompatActivity() {
+
+    private var handler: Handler? = null
+
+    private val runnable = Runnable {
+
+        // TODO: 遷移先を分離？
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        handler = Handler()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        handler?.postDelayed(runnable, 800)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        handler?.removeCallbacks(runnable)
     }
 }
