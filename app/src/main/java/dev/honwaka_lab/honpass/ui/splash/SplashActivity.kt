1e5ad431098f323ab1c7a4a079959f5d2725ba05
Honwaka Developer <honwaka.developer@gmail.com>
2019-08-20 11:29:19 +0900

    SplashActivityにinternal修飾子を付与

--- app/src/main/java/dev/honwaka_lab/honpass/ui/splash/SplashActivity.kt
+++ app/src/main/java/dev/honwaka_lab/honpass/ui/splash/SplashActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dev.honwaka_lab.honpass.R
 
-class SplashActivity : AppCompatActivity() {
+internal class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
