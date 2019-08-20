c0d9957b026a6be1288fa54e081f624eef132fd9
Honwaka Developer <honwaka.developer@gmail.com>
2019-08-20 11:29:54 +0900

    登録用のActivityを作成

---
+++ app/src/main/java/dev/honwaka_lab/honpass/ui/register/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package dev.honwaka_lab.honpass.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.honwaka_lab.honpass.R
+
+internal class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
