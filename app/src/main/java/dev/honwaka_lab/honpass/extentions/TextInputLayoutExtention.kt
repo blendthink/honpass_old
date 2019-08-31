4ba15cf67eaad4aaf3f455fb63ef38089d86896d
Honwaka Developer <honwaka.developer@gmail.com>
2019-08-31 16:44:52 +0900

    ErrorTextからErrorMessageに修正

--- app/src/main/java/dev/honwaka_lab/honpass/extentions/TextInputLayoutExtention.kt
+++ app/src/main/java/dev/honwaka_lab/honpass/extentions/TextInputLayoutExtention.kt
@@ -3,7 +3,7 @@ package dev.honwaka_lab.honpass.extentions
 import androidx.databinding.BindingAdapter
 import com.google.android.material.textfield.TextInputLayout
 
-@BindingAdapter("errorText")
-fun TextInputLayout.setErrorText(errorText: String?) {
-    error = errorText
+@BindingAdapter("errorMessage")
+fun TextInputLayout.setErrorMessage(errorMessage: String?) {
+    error = errorMessage
 }
