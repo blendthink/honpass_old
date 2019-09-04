db93c5834a7d4c10800f5cfbdf063ee3d44fd9c2
Honwaka Developer <honwaka.developer@gmail.com>
2019-09-04 11:11:33 +0900

    パスワードの入力誤りのメッセージを修正

--- app/src/main/java/dev/honwaka_lab/honpass/ui/register/model/PasswordForConfirmError.kt
+++ app/src/main/java/dev/honwaka_lab/honpass/ui/register/model/PasswordForConfirmError.kt
@@ -2,6 +2,6 @@ package dev.honwaka_lab.honpass.ui.register.model
 
 internal enum class PasswordForConfirmError(val message: String?) {
     NONE(null),
-    PASSWORD_ERROR("※ パスワードの入力謝り"),
+    PASSWORD_ERROR("※ パスワードの入力誤り"),
     DIFFERENT("※ パスワードの不一致")
 }
