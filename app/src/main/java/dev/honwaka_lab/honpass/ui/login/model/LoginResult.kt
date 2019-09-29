294377bbb1bc80564afd89eba41cfa1817b1ad0e
Honwaka Developer <honwaka.developer@gmail.com>
2019-09-29 21:43:46 +0900

    不要になったファイルを削除

--- app/src/main/java/dev/honwaka_lab/honpass/ui/login/model/LoginResult.kt
+++
@@ -1,8 +0,0 @@
-package dev.honwaka_lab.honpass.ui.login.model
-
-/**
- * Authentication isRegisteredResult : success (user details) or error message.
- */
-internal data class LoginResult(
-    val error: Int? = null
-)
