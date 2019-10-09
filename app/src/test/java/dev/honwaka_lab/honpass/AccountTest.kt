847b3c7066ad908853e4e30c8238825657aa2832
Honwaka Developer <honwaka.developer@gmail.com>
2019-10-10 00:19:44 +0900

    Account のテストクラスを仮に作成

---
+++ app/src/test/java/dev/honwaka_lab/honpass/AccountTest.kt
@@ -0,0 +1,12 @@
+package dev.honwaka_lab.honpass
+
+import org.junit.Assert
+import org.junit.Test
+
+class AccountTest {
+
+    @Test
+    fun testAccount() {
+        Assert.assertEquals(1, 1 + 2)
+    }
+}
