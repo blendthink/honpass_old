8aafe1fd667c3bb706799a77841419e017aa6985
Honwaka Developer <honwaka.developer@gmail.com>
2019-10-10 00:41:24 +0900

    テストを修正

--- app/src/test/java/dev/honwaka_lab/honpass/ExampleUnitTest.kt
+++ app/src/test/java/dev/honwaka_lab/honpass/ExampleUnitTest.kt
@@ -11,11 +11,11 @@ import org.junit.Test
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(2, 2 + 2)
+        assertEquals(3, 2 + 2)
     }
-    
+
     @Test
     fun addition() {
-        assertEquals(9, 2 + 2)
+        assertEquals(4, 2 + 2)
     }
 }
